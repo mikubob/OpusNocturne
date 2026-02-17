db930a307b39cbe0b1e35dc2ca864ca160b51b76
mikubob <2386782347@qq.com>
2026-02-17 19:51:48 +0800

    refactor(config): 将Knife4j配置从service模块迁移到common模块并优化配置结构
    - 将Knife4jConfig从ON-service模块移动到ON-common模块
    - 使用@ConfigurationProperties替代硬编码的API文档配置信息
    - 添加Knife4jProperties配置类来管理API文档的基本信息
    - 移除原有的API分组配置功能
    - 统一管理联系人配置属性结构

---
+++ OpusNocturne/ON-common/src/main/java/com/xuan/common/config/Knife4jConfig.java
@@ -0,0 +1,67 @@
+package com.xuan.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * knife4j配置类 - OpenAPI3.0
+ *
+ * @author 玄〤
+ * @since 2026-02-17
+ */
+@Configuration
+public class Knife4jConfig {
+
+    /**
+     * OpenAPI基本信息配置
+     */
+    @Bean
+    public OpenAPI customOpenAPI(Knife4jProperties properties) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(properties.getTitle())
+                        .description(properties.getDescription())
+                        .version(properties.getVersion())
+                        .contact(new Contact()
+                                .name(properties.getContact().getName())
+                                .email(properties.getContact().getEmail())
+                                .url(properties.getContact().getUrl())));
+    }
+
+    /**
+     * Knife4j配置属性类
+     */
+    @Bean
+    @ConfigurationProperties(prefix = "knife4j")
+    public Knife4jProperties knife4jProperties() {
+        return new Knife4jProperties();
+    }
+
+    /**
+     * Knife4j配置属性类
+     */
+    @Data
+    public static class Knife4jProperties {
+        private String title = "OpusNocturne博客系统API文档";
+        private String description = "基于Spring Boot 3.4.2 和 JDK21的现代化博客系统";
+        private String version = "1.0.0";
+        private ContactProperties contact = new ContactProperties();
+
+        /**
+         * 联系人配置属性
+         */
+        @Data
+        public static class ContactProperties {
+            private String name = "玄〤";
+            private String email = "2386782347@qq.com";
+            private String url = "https://github.com/mikubob/OpusNocturne";
+
+        }
+    }
+
+}
